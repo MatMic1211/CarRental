6fbb772a52bedfe152cb3464a0d5ad6d42661cb0
MatMic1211 <mat.micz@wp.pl>
2025-06-25 17:55:21 +0200

    Start

--- Pointman.CarRental.Web/pointman.carrental.web.client/src/Common/About/about.component.ts
+++ Pointman.CarRental.Web/pointman.carrental.web.client/src/Common/About/about.component.ts
@@ -12,12 +12,12 @@ export class AboutComponent {
 
 
   trustedCompanies = [
-    { name: 'Fazi Cars', logo: 'assets/Images/png/fazi_cars_logo.png' },
-    { name: 'Mago Cars', logo: 'assets/Images/png/mago_cars_logo.png' },
-    { name: 'Race Cars 24', logo: 'assets/Images/png/race_cars_24_logo.png' },
-    { name: 'Spectrum Cars', logo: 'assets/Images/png/spcetrum_cars_logo.png' },
-    { name: 'Trofeo', logo: 'assets/Images/png/trofeo_logo.png' },
-    { name: 'Bad Cars', logo: 'assets/Images/png/bad_cars_log.png' }
+    {logo: 'assets/Images/png/fazi_cars_logo.png' },
+    { logo: 'assets/Images/png/mago_cars_logo.png' },
+    {logo: 'assets/Images/png/race_cars_24_logo.png'},
+    {logo: 'assets/Images/png/spcetrum_cars_logo.png'},
+    {logo: 'assets/Images/png/trofeo_logo.png'},
+    {logo: 'assets/Images/png/bad_cars_log.png'}
   ];
 
   getTranslation(key: string): string {
